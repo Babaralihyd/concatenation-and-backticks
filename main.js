5549e5438c3774cd41d4f13ef328c828cf77dda9
Babaralihyd <162362468+Babaralihyd@users.noreply.github.com>
2024-03-12 13:53:11 -1200

    concatenation and template  literals backticks

---
+++ main.js
@@ -0,0 +1,25 @@
+// concatention 
+console.log('hellow' + 'world '); // concatention 
+console.log("2" + 2); // concatention 
+console.log(2 + 2); //  not concatention this is arithmatic operation
+// addition  
+var num1 = 23;
+var num2 = 50;
+var sum = num1 + num2;
+console.log('the sum of ' + num1 + ' and ' + num2 + ' will be ' + sum); // concatention 
+//subtration 
+var num3 = 278;
+var num4 = 12;
+console.log(num3 - num4);
+// multipulication 
+var num5 = 10;
+var num6 = 12;
+console.log(num5 * num6);
+// division   
+var num7 = 30;
+var num8 = 5;
+console.log(num7 / num8);
+//template literals (backtick ~`)
+console.log("the sum of  ".concat(num1, " and ").concat(num2, " will be  ").concat(sum, " ")); //${num1}-->  place holders
+var personName = 'imran';
+console.log("mr ".concat(personName, " is good person and his beatiful"));
---
+++ main.ts
@@ -0,0 +1,36 @@
+// concatention 
+console.log('hellow' + 'world ') // concatention 
+console.log ("2" + 2) // concatention 
+console.log ( 2 +2 ) //  not concatention this is arithmatic operation
+   // addition  
+let num1= 23; 
+let num2 = 50; 
+let sum =  num1 + num2 ; 
+console.log ('the sum of ' + num1 + ' and '+ num2 + ' will be ' + sum ) ;  // concatention 
+
+//subtration 
+
+let num3= 278; 
+let num4 = 12; 
+console.log   ( num3 - num4 )
+
+// multipulication 
+let num5= 10; 
+let num6 = 12; 
+console.log   ( num5 * num6);
+ 
+// division   
+
+const num7= 30;
+const num8 =5;
+console.log   ( num7 / num8);
+
+
+//template literals (backtick ~`)
+
+console.log (`the sum of  ${ num1} and ${num2} will be  ${sum } `)  //${num1}-->  place holders
+
+let personName = 'imran'; 
+console.log (`mr ${personName} is good person and his beatiful` ) 
+
+
